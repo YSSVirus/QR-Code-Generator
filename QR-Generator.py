cc8a5d84d775dc4ec41402050266b59e89c91ac7
YSSVirus <56813561+YSSVirus@users.noreply.github.com>
2026-07-13 01:53:36 -0400

    Update QR-Generator.py

--- QR-Generator.py
+++ QR-Generator.py
@@ -45,25 +45,32 @@ arg_items = arg_parser.parse_args()
 
 
 # Core Functions
-def create_qr_code(website, file_fullname, color_front=(255, 255, 255), color_back=(1, 0, 0), color_pattern_selected="SolidFillColorMask"):
-        qr = qrcode.QRCode(
-                error_correction=qrcode.ERROR_CORRECT_H,
-                image_factory=styledpil.StyledPilImage
-        )
+def create_qr_code(url, file_path_qr):
+                
+                qr = qrcode.QRCode(error_correction=qrcode.ERROR_CORRECT_H, image_factory=styledpil.StyledPilImage)
+                qr.add_data(url)
+                qr.make(fit=True)
+                
+                # Prevent the library blending bug on pure black backgrounds
+                render_bg = color_background
+                if render_bg == (0, 0, 0):
+                        render_bg = (1, 0, 0)
+                
+                if color_pattern_selected == "SolidFillColorMask":
+                        color_mask = colormasks.SolidFillColorMask(back_color=render_bg, front_color=color_foreground)
+                elif color_pattern_selected == "HorizontalGradiantColorMask":
+                        color_mask = colormasks.HorizontalGradiantColorMask(back_color=render_bg, left_color=color_left, right_color=color_right)
+                elif color_pattern_selected == "VerticalGradiantColorMask":
+                        color_mask = colormasks.VerticalGradiantColorMask(back_color=render_bg, top_color=color_top, bottom_color=color_bottom)
+                else:
+                        color_mask = colormasks.RadialGradiantColorMask(back_color=render_bg, center_color=color_center, edge_color=color_edge)
 
-        qr.add_data(website)
-        qr.make(fit=True)
-        
-        if color_pattern_selected == "SolidFillColorMask":
-                color_mask = colormasks.SolidFillColorMask(back_color=color_back, front_color=color_front)
-        elif color_pattern_selected == "HorizontalGradiantColorMask":
-                color_mask = colormasks.HorizontalGradiantColorMask(back_color=color_back, left_color=color_front, right_color=color_right)
-        elif color_pattern_selected == "VerticalGradiantColorMask":
-                color_mask = colormasks.VerticalGradiantColorMask(back_color=color_back, top_color=color_front, bottom_color=color_bottom)
-        else:
-                color_mask = colormasks.RadialGradiantColorMask(back_color=color_back, center_color=color_front, edge_color=color_edge)
+                if image_center_path == None:
+                        img = qr.make_image(color_mask=color_mask, module_drawer=shape_pattern_selected)
+                else:
+                        img = qr.make_image(color_mask=color_mask, module_drawer=shape_pattern_selected, embeded_image_path=image_center_path)
 
-        img.save(file_fullname)
+                img.save(file_path_qr)
 
 def app_gui():
         global color_pattern_selected
@@ -76,19 +83,83 @@ def app_gui():
         global color_right
         global color_center
         global color_edge
-        global file_qr_code
         global theme_mode
         global theme_icon
         global shape_pattern
         global image_center_path
 
-        color_top = (1, 0, 0)
+
+        
         color_bottom = (255, 255, 255)
         color_left = (1, 0, 0)
         color_right = (255, 255, 255)
         color_center = (1, 0, 0)
         color_edge = (255, 255, 255)
+        
         image_center_path = None
+        
+        color_pattern_selected = "SolidFillColorMask"
+        root = Window(themename="darkly")
+
+        website = tk.StringVar()
+        file_name = tk.StringVar()
+
+        shape_pattern_selected = moduledrawers.SquareModuleDrawer()
+        
+        options_list_color_pattern = {
+            "Solid": (
+                "SolidFillColorMask",
+                "solid.png"
+            ),
+            "Radial Gradiant": (
+                "RadialGradiantColorMask",
+                "radial-gradiant.png"
+            ),
+            "Square Gradiant": (
+                "RadialGradiantColorMask",
+                "square-gradiant.png"
+            ),
+            "Horizontal Gradiant": (
+                "HorizontalGradiantColorMask",
+                "horizontal-gradiant.png"
+            ),
+            "Vertical Gradiant": (
+                "VerticalGradiantColorMask",
+                "vertical-gradiant.png"
+            )
+        }
+
+        options_list_shape_pattern = {
+            "Squares": (
+                moduledrawers.SquareModuleDrawer(),
+                "squares.png"
+            ),
+            "Small Squares": (
+                moduledrawers.GappedSquareModuleDrawer(),
+                "small-squares.png"
+            ),
+            "Rounded Squares": (
+                moduledrawers.RoundedModuleDrawer(),
+                "rounded_squares.png"
+            ),
+            "Circles": (
+                moduledrawers.CircleModuleDrawer(),
+                "circles.png"
+            ),
+            "Horizontal Bars": (
+                moduledrawers.HorizontalBarsDrawer(),
+                "horizontal-bars.png"
+            ),
+            "Vertical Bars": (
+                moduledrawers.VerticalBarsDrawer(),
+                "vertical-bars.png"
+            )
+        }
+
+        options_color_pattern = list(options_list_color_pattern.keys())
+        options_shape_pattern = list(options_list_shape_pattern.keys())
+
+
 
         def action_toggle_theme():
                 global theme_mode
@@ -105,18 +176,21 @@ def app_gui():
 
         def action_picker_color_background():
                 global color_background
+                color_background = (1, 0, 0)
                 color_background_choice = colorchooser.askcolor(title = "Choose color")
                 if color_background_choice[0]:
                         color_background = tuple(int(c) for c in color_background_choice[0])
 
         def action_picker_color_foreground():
                 global color_foreground
+                color_foreground = (255, 255, 255)
                 color_foreground_choice = colorchooser.askcolor(title = "Choose color")
                 if color_foreground_choice[0]:
                         color_foreground = tuple(int(c) for c in color_foreground_choice[0])
 
         def action_picker_color_top():
                 global color_top
+                color_top = (1, 0, 0)
                 choice = colorchooser.askcolor(title="Choose Top Color")
                 if choice[0]:
                         color_top = tuple(int(c) for c in choice[0])
@@ -222,11 +296,8 @@ def app_gui():
                         button_center_image.grid(row=8, column=0, pady=10)
                                                 
         def action_submit():
-                global color_background
-                global color_foreground
-                global color_pattern
                 global file_qr_code
-                
+                file_qr_code = ""
                 website_cleaned = website.get()
                 file_name_cleaned = file_name.get()
                 file_ext = ".png"
@@ -240,97 +311,13 @@ def app_gui():
                         ttk.Label(win, text="A Url is REQUIRED!").pack(expand=True)
                         win.after(2500, win.destroy)
                         return
-
-                qr = qrcode.QRCode(error_correction=qrcode.ERROR_CORRECT_H, image_factory=styledpil.StyledPilImage)
-                qr.add_data(website_cleaned)
-                qr.make(fit=True)
                 
-                # Prevent the library blending bug on pure black backgrounds
-                render_bg = color_background
-                if render_bg == (0, 0, 0):
-                        render_bg = (1, 0, 0)
-                
-                if color_pattern_selected == "SolidFillColorMask":
-                        color_mask = colormasks.SolidFillColorMask(back_color=render_bg, front_color=color_foreground)
-                elif color_pattern_selected == "HorizontalGradiantColorMask":
-                        color_mask = colormasks.HorizontalGradiantColorMask(back_color=render_bg, left_color=color_left, right_color=color_right)
-                elif color_pattern_selected == "VerticalGradiantColorMask":
-                        color_mask = colormasks.VerticalGradiantColorMask(back_color=render_bg, top_color=color_top, bottom_color=color_bottom)
-                else:
-                        color_mask = colormasks.RadialGradiantColorMask(back_color=render_bg, center_color=color_center, edge_color=color_edge)
-
-                if image_center_path == None:
-                        img = qr.make_image(color_mask=color_mask, module_drawer=shape_pattern_selected)
-                else:
-                        img = qr.make_image(color_mask=color_mask, module_drawer=shape_pattern_selected, embeded_image_path=image_center_path)
-
-                img.save(file_fullname)
-                
+                create_qr_code(website_cleaned, file_fullname)
                 file_qr_code = ImageTk.PhotoImage(Image.open(file_fullname).resize((350, 350)))
                 label_image = Label(root, image = file_qr_code)
-                label_image.grid(row=9, column=2, columnspan=3, pady=10)
+                label_image.grid(row=9, column=0, columnspan=5, pady=10)
 
-        root = Window(themename="darkly")
-        website = tk.StringVar()
-        file_name = tk.StringVar()
-        file_qr_code = ""
-        color_background = (1, 0, 0)
-        color_foreground = (255, 255, 255)
-        color_pattern_selected = "SolidFillColorMask"
-        shape_pattern_selected = moduledrawers.SquareModuleDrawer()
-        
-        options_list_color_pattern = {
-            "Solid": (
-                "SolidFillColorMask",
-                "solid.png"
-            ),
-            "Radial Gradiant": (
-                "RadialGradiantColorMask",
-                "radial-gradiant.png"
-            ),
-            "Square Gradiant": (
-                "RadialGradiantColorMask",
-                "square-gradiant.png"
-            ),
-            "Horizontal Gradiant": (
-                "HorizontalGradiantColorMask",
-                "horizontal-gradiant.png"
-            ),
-            "Vertical Gradiant": (
-                "VerticalGradiantColorMask",
-                "vertical-gradiant.png"
-            )
-        }
 
-        options_list_shape_pattern = {
-            "Squares": (
-                moduledrawers.SquareModuleDrawer(),
-                "squares.png"
-            ),
-            "Small Squares": (
-                moduledrawers.GappedSquareModuleDrawer(),
-                "small-squares.png"
-            ),
-            "Rounded Squares": (
-                moduledrawers.RoundedModuleDrawer(),
-                "rounded_squares.png"
-            ),
-            "Circles": (
-                moduledrawers.CircleModuleDrawer(),
-                "circles.png"
-            ),
-            "Horizontal Bars": (
-                moduledrawers.HorizontalBarsDrawer(),
-                "horizontal-bars.png"
-            ),
-            "Vertical Bars": (
-                moduledrawers.VerticalBarsDrawer(),
-                "vertical-bars.png"
-            )
-        }
-
-        options_color_pattern = list(options_list_color_pattern.keys())
-        options_shape_pattern = list(options_list_shape_pattern.keys())
 
         mask_color_pattern_mappings = {
             name: mask
@@ -362,6 +349,8 @@ def app_gui():
         theme_mode = "dark"
         theme_icon = "☀ Light Mode"
 
+        
+
         label_required_fields = ttk.Label(root, text = "fields marked with * are REQUIRED", font=('calibre',8,'normal'))
 
         button_theme = tk.Button(root, text = theme_icon, command = action_toggle_theme)
@@ -380,7 +369,6 @@ def app_gui():
         button_color_right = ttk.Button(root, text = "Select Right color", command = action_picker_color_right)
         button_color_center = ttk.Button(root, text = "Select Center color", command = action_picker_color_center)
         button_color_edge = ttk.Button(root, text = "Select Edge color", command = action_picker_color_edge)
-
 
         label_pattern_choices = ttk.Label(root, text = 'Pattern Choices', font=('calibre',12,'normal'))
         button_dropdown_color_pattern = tk.Menubutton(
@@ -419,27 +407,40 @@ def app_gui():
                         command=lambda n=name: on_select(n, shape_pattern[n], mask_shape_pattern_mappings[n], "pattern_shape")
                 )
 
+
+
         button_center_image = tk.Button(root, text = "Select Center Image", command = action_select_image_prompt)
 
         button_submit = ttk.Button(root,text = "Create QR Code", command = action_submit)
 
-        label_required_fields.grid(row=0, column=0, pady=10)
-        button_theme.grid(row=0, column=4, pady=10)
-        label_name.grid(row=1, column=1)
-        input_name.grid(row=1, column=2)
-        label_website.grid(row=2, column=1, pady=10)
-        input_website.grid(row=2, column=2)
+        label_required_fields.grid(row=0, column=0, pady=10, padx=10, sticky="w")
+        button_theme.grid(row=0, column=4, pady=10, padx=10, sticky="e")
+
+        label_name.grid(row=1, column=1, sticky="e")
+        input_name.grid(row=1, column=2, sticky="ew", padx=(5, 10))
+
+        label_website.grid(row=2, column=1, pady=10, sticky="e")
+        input_website.grid(row=2, column=2, sticky="ew", padx=(5, 10))
 
         label_color_choices.grid(row=1, column=0, pady=10)
+
         button_color_background.grid(row=2, column=0)
+
         button_color_foreground.grid(row=3, column=0)
 
-
         label_pattern_choices.grid(row=4, column=0, pady=10)
+
         button_dropdown_color_pattern.grid(row=5, column=0, pady=10)
+
         button_dropdown_shape_pattern.grid(row=6, column=0, pady=10)
+
         button_center_image.grid(row=7, column=0, pady=10)
-        button_submit.grid(row=8, column=3)
+
+        button_submit.grid(row=8, column=2, sticky="e", padx=(0, 10))
+
+        root.columnconfigure(2, weight=1)
+        root.columnconfigure(4, weight=2)
+        root.rowconfigure(9, weight=1)
 
         frm.grid()
         root.resizable(True, True)
